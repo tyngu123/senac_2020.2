db3c3017f28cf4a2c3241419375359d5f3408bec
Carlos <carlos.eduardo040902@gmail.com>
2021-08-13 21:26:23 -0300

    aula de função

--- angular/Carlos_eduardo/app.ts
+++ angular/Carlos_eduardo/app.ts
@@ -8,3 +8,28 @@ let bairro = "centro"
 
 console.log("O nome é : " + nome);
 console.log("A idade é : " + idade);
+
+
+//Função sem parametro e sem retorno//
+function qualNome(): void{
+    console.log("executei a função!!");
+}
+
+
+//Função com parametro e sem retorno//
+function somar(numero1: number, numero2: number): void{
+    console.log("a soma é : " + (numero1 + numero2));
+}
+
+
+
+function temMaisTitulo(valor: number): boolean{
+    return valor > 30;
+}
+
+
+
+qualNome();
+somar(5, 2);
+console.log("Esse time tem mais titulos que o flamengo? " + temMaisTitulo(45));
+
