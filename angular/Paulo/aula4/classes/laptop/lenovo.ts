069fd3ebb06bd1e0c2c72c06177f8fa54e25c12a
PauloVitorPaixao <paulovitorpro021@gmail.com>
2021-08-18 23:11:57 -0300

    Aula4 - Organização de pastas

---
+++ angular/Paulo/aula4/classes/laptop/lenovo.ts
@@ -0,0 +1,11 @@
+import { Laptop } from './laptop';
+
+export class Lenovo extends Laptop {
+    constructor (tamanho: number){
+        super(tamanho);
+    }
+
+    aumentarBrilho(valor: number){
+        console.log(`O brilho aumentou em ${valor} pontos!`);
+    }
+}
---
+++ angular/Paulo/aula4/classes/laptop/lenovox.ts
@@ -0,0 +1,10 @@
+import { Laptop } from './laptop';
+import { Gamer } from '../interface/gamer';
+
+export class LenovoX extends Laptop implements Gamer {
+    memoriaRAM: number;
+
+    constructor (tamanho: number) {
+        super(tamanho);
+    }
+}
